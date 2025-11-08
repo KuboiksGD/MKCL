0f03e39150c12f3d29827a1f112227fb32b43178
KuboiksGD <subcountry15@gmail.com>
2025-11-08 12:02:53 +0100

    s

---
+++ js/pages/List.js
@@ -0,0 +1,207 @@
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../content.js";
+
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
+
+const roleIconMap = {
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
+};
+
+export default {
+    components: { Spinner, LevelAuthors },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-list">
+            <div class="list-container">
+                <input
+                    v-model="search"
+                    type="search"
+                    placeholder="Szukaj poziomów..."
+                    class="search"
+                />
+                <table class="list" v-if="filteredList.length">
+                    <tr v-for="([level, err], i) in filteredList" :key="level?.id || i">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 50" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+                <p v-else>No levels found.</p>
+            </div>
+            <div class="level-container">
+                <div class="level" v-if="level">
+                    <h1>{{ level.name }}</h1>
+                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
+                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
+                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ level.id }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">Password</div>
+                            <p>{{ level.password || 'Free to Copy' }}</p>
+                        </li>
+                    </ul>
+                    <h2>Records</h2>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
+                    <p v-else>This level does not accept new records.</p>
+                    <table class="records">
+                        <tr v-for="record in level.records" class="record" :key="record.user + record.percent">
+                            <td class="percent">
+                                <p>{{ record.percent }}%</p>
+                            </td>
+                            <td class="user">
+                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                            </td>
+                            <td class="mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
+                            </td>
+                            <td class="hz">
+                                <p>{{ record.hz }}Hz</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                </div>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors" :key="error">{{ error }}</p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>Edytorzy listy</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors" :key="editor.name">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>Zasady dodawania leveli</h3>
+                    <p>
+                        Nie można używać hacków.
+                    </p>
+                    <p>
+                        Cbf jest dozwolony poniżej 144 fps/hz
+                    </p>
+                    <p>
+                        Nagranie musi posiadać clicki lub tapy.
+                    </p>
+                    <p>
+                        nagranie musi pokazywać całe przejście i poprzedni death.
+                    </p>
+                    <p>
+                        na nagraniu musi widnieć endscreen
+                    </p>
+                    <p>
+                        Nie można uzywać secret way i swag routów.
+                    </p>
+                    <p>
+                        Jeżeli level spadnie do legacy nie akceptujemy nagrania.
+                    </p>
+                    <p>
+                        Level musi mieć ponizej 31sekund.
+                    </p>
+                       <p>
+                        minimalne deco levela tak jak na schabek challange.
+                    </p>
+                </div>
+            </div>
+        </main>
+    `,
+    data: () => ({
+        list: [],
+        editors: [],
+        loading: true,
+        selected: 0,
+        errors: [],
+        roleIconMap,
+        store,
+        search: "",
+    }),
+    computed: {
+        level() {
+            return this.filteredList[this.selected]?.[0];
+        },
+        video() {
+            if (!this.level?.showcase) {
+                return embed(this.level?.verification);
+            }
+
+            return embed(
+                this.toggledShowcase
+                    ? this.level.showcase
+                    : this.level.verification
+            );
+        },
+        filteredList() {
+            if (!this.search) return this.list;
+            const searchLower = this.search.toLowerCase();
+            return this.list.filter(([level]) => {
+                if (!level) return false;
+                return level.name.toLowerCase().includes(searchLower);
+            });
+        },
+    },
+    watch: {
+        filteredList(newList) {
+            if (newList.length > 0) {
+                this.selected = 0;
+            }
+        }
+    },
+    async mounted() {
+        // Hide loading spinner
+        this.list = await fetchList();
+        this.editors = await fetchEditors();
+
+        // Error handling
+        if (!this.list) {
+            this.errors = [
+                "Failed to load list. Retry in a few minutes or notify list staff.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => {
+                        return `Failed to load level. (${err}.json)`;
+                    })
+            );
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
+        }
+
+        this.loading = false;
+    },
+    methods: {
+        embed,
+        score,
+    },
+};
